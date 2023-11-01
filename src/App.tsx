e56eb9b3833d3aa851436f75ec9e12f85ed501b9
samuel <shemlouk@gmail.com>
2023-11-01 19:07:35 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="font-[Roboto] w-screen h-screen bg-charcoalGrey"></div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        darkSlateGrey: "hsl(234, 29%, 20%)",
+        charcoalGrey: "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
+      },
+    },
+  },
+  plugins: [],
+};
