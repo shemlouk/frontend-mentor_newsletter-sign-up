dbe6508866813862770621d3be303a285c9f6a34
samuel <shemlouk@gmail.com>
2023-11-02 21:02:47 -0300

    feat: add input validation to form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { signal } from "@preact/signals-react";
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import IllustrationSignUpDesktop from "./assets/images/illustration-sign-up-desktop.svg";
 import IllustrationSignUpMobile from "./assets/images/illustration-sign-up-mobile.svg";
 import Button from "./components/Button";
@@ -11,7 +13,19 @@ window.addEventListener("resize", () => {
   screenWidth.value = window.innerWidth;
 });
 
+const email = signal("");
+
 function App() {
+  const navigate = useNavigate();
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (email.value) navigate("/done");
+    },
+    [navigate]
+  );
+
   return (
     <div className="font-[Roboto] w-full h-screen bg-charcoalGrey flex text-darkSlateGrey justify-center items-center">
       <div className="bg-white h-full w-full flex gap-10 flex-col md:h-[600px] md:w-[900px] md:rounded-3xl md:flex-row-reverse md:items-center md:p-6">
@@ -35,18 +49,23 @@ function App() {
             <ListItem text="And much more!" />
           </ul>
 
-          <form className="flex flex-col gap-6">
-            <label htmlFor="email" className="font-bold text-sm translate-y-4">
-              Email address
-            </label>
-            <input
-              id="email"
-              required
-              type="email"
-              placeholder="email@company.com"
-              className="rounded-lg border border-grey p-4 placeholder:text-grey outline-none"
+          <form onSubmit={handleSubmit} className="flex flex-col gap-6 mt-4">
+            <fieldset className="flex flex-col gap-2 invalid:before:content-['Valid_email_required'] before:text-sm before:text-red-500 before:font-bold relative before:absolute before:right-0">
+              <label htmlFor="email" className="font-bold text-sm">
+                Email address
+              </label>
+              <input
+                id="email"
+                type="email"
+                placeholder="email@company.com"
+                onChange={(e) => (email.value = e.currentTarget.value)}
+                className="rounded-lg border border-grey p-4 placeholder:text-grey outline-none invalid:bg-red-50 invalid:border-red-500 invalid:text-red-500 invalid:placeholder:text-red-500"
+              />
+            </fieldset>
+            <Button
+              text="Subscribe to monthly newsletter"
+              disabled={!email.value}
             />
-            <Button text="Subscribe to monthly newsletter" />
           </form>
         </div>
       </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,15 @@
-function Button({ text }: { text: string }) {
+type ButtonProps = { text?: string } & React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+function Button(props: ButtonProps) {
   return (
-    <button className="font-bold bg-darkSlateGrey rounded-lg p-4 w-full text-white transition hover:bg-gradient-to-r hover:from-[#FF527C] hover:to-[#FF6A39] relative hover:before:absolute hover:before:w-full hover:before:h-full hover:before:bg-black hover:before:top-0 hover:before:left-0 z-10 hover:before:-z-10 hover:before:bg-gradient-to-r hover:before:from-[#FF527C] hover:before:to-[#FF6A39] hover:before:blur">
-      {text}
+    <button
+      {...props}
+      className="font-bold bg-darkSlateGrey cursor-pointer rounded-lg disabled:bg-charcoalGrey disabled:cursor-not-allowed p-4 w-full text-white transition enabled:hover:bg-gradient-to-r enabled:hover:from-[#FF527C] enabled:hover:to-[#FF6A39] relative enabled:hover:before:absolute enabled:hover:before:w-full enabled:hover:before:h-full enabled:hover:before:bg-black enabled:hover:before:top-0 enabled:hover:before:left-0 z-10 enabled:hover:before:-z-10 enabled:hover:before:bg-gradient-to-r enabled:hover:before:from-[#FF527C] enabled:hover:before:to-[#FF6A39] enabled:hover:before:blur"
+    >
+      {props.text}
     </button>
   );
 }
